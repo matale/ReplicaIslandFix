eea768c8b9d489f8e9c224e230c5dec157a0f639
Roman <roman.mbwasi@later.com>
2025-06-21 20:54:33 -0400

    SDK Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.replica.replicaisland"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.replica.replicaisland"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
